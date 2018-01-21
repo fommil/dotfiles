2617a4926f75573c8ff513e96c159131302f0669
Sam Halliday <sam.halliday@gmail.com>
2018-01-21 15:24:35 +0000

    words, and stuff

--- Profiles/Samurai/.emacs.d/local.el
+++ Profiles/Samurai/.emacs.d/local.el
@@ -12,7 +12,8 @@
 
 ;; I'll usually want access to these..
 ;;(find-file (expand-file-name "scratch.el" user-emacs-directory))
-;;(find-file "~/Projects/fpmortals/manuscript/book.org")
+(find-file "~/Projects/scalaz/core/src/main/scala/scalaz")
+(find-file "~/Projects/fpmortals/manuscript/book.org")
 ;;(find-file "~/Work")
 ;;(switch-to-buffer "*scratch*")
 
