f8c1bf758af009f3a8f6990b8417dfda0c31ef7a
Sam Halliday <sam.halliday@gmail.com>
2019-05-06 13:28:34 +0100

    haskell opts

--- .emacs.d/lisp/fommil-haskell-tng.el
+++ .emacs.d/lisp/fommil-haskell-tng.el
@@ -9,15 +9,29 @@
 ;;
 ;;  TODO send some PRs to haskell-tng
 ;;
-;;  1. indentation for WLDO almost always gets it wrong
+;;  1. indentation for WLDO almost always gets it wrong. Should be matching on the last inferred ; unless it's the first one, in which case it should be where +2
 ;;  2. alternative indentation should prioritise the previous line and next line
-;;  3. defining top-levels are terrible, lines following a type defn should be top
-;;     level unless there is a -> or =>
+;;  3. top-levels should be 0 if the previous line is the opener, unless there is a -> or =>
 ;;  4. GADT definitions are bad, seems to prefer the :: not the entries
-;;  5. why is sp-wrap-with-pair doing a re-indent?
-;;  6. tag lookup on a FQN.symbol picks up the wrong thing
+;;  5. tag lookup on a FQN.symbol picks up the wrong thing
 ;;
 ;;; Code:
+
+(require 'smartparens)
+;; WORKAROUND smartparens is indenting all the time, which is not good
+(defun sp--indent-region (_1 _2 &optional _3)
+  ;; disable this function
+  )
+
+;; (defun sp--indent-region (start end &optional column)
+;;   "Call `indent-region' unless `aggressive-indent-mode' is enabled.
+
+;; START, END and COLUMN are the same as in `indent-region'."
+;;   (unless (bound-and-true-p aggressive-indent-mode)
+;;     ;; Don't issue "Indenting region..." message.
+;;     (cl-letf (((symbol-function 'message) #'ignore))
+;;       (indent-region start end column))))
+
 
 (add-to-load-path "~/Projects/haskell-tng.el")
 (require 'haskell-tng-mode)
@@ -36,9 +50,9 @@
 
    (setq company-backends (company-backends-for-buffer))))
 
-(bind-key "C-c c" 'haskell-compile haskell-tng-mode-map)
+(bind-key "C-c c" 'haskell-tng-compile haskell-tng-mode-map)
 (bind-key "C-c e" 'next-error haskell-tng-mode-map)
-(bind-key "C-c c" 'haskell-compile haskell-tng-compilation-mode-map)
+(bind-key "C-c c" 'haskell-tng-compile haskell-tng-compilation-mode-map)
 (bind-key "C-c e" 'next-error haskell-tng-compilation-mode-map)
 
 (bind-key "C-c C-r f" 'haskell-tng-contrib:stylish-haskell haskell-tng-mode-map)
