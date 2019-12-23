a894900be9d0a34fed86d4111b84528fcfdfc31a
Sam Halliday <sam.halliday@gmail.com>
2019-12-23 09:50:33 +0000

    user name is included in Profile name

---
+++ Profiles/Samurai-symbiont/.emacs.d/local.el
@@ -0,0 +1,28 @@
+;;; local.el --- Local config
+;;; Commentary:
+;;
+;;  For desktop.
+;;
+;;; Code:
+
+(add-to-list 'default-frame-alist
+             '(font . "Hack-18"))
+
+(when (not window-system)
+  (add-to-list 'default-frame-alist
+               '(cursor-type . box)))
+;; (add-to-list 'default-frame-alist
+;;              '(font . "Hack-24"))
+
+
+(find-file (expand-file-name "scratch.el" user-emacs-directory))
+
+(find-file "~/Work/")
+
+(light-theme)
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars unresolved)
+;; End:
+
+;;; local.el ends here
