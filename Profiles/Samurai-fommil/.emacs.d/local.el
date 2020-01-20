f5be7446b2b95927ee06e2d4943b399598e35692
Sam Halliday <sam.halliday@gmail.com>
2020-01-20 11:22:09 +0000

    trying a new font

--- Profiles/Samurai-fommil/.emacs.d/local.el
+++ Profiles/Samurai-fommil/.emacs.d/local.el
@@ -6,7 +6,9 @@
 ;;; Code:
 
 (add-to-list 'default-frame-alist
-             '(font . "Hack-18"))
+             '(font . "JetBrainsMono-18"))
+
+(package-ensure-compiled)
 
 ;; I'll usually want access to these..
 (find-file (expand-file-name "scratch.el" user-emacs-directory))
