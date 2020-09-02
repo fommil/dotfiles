aff5f2c0dedf8fa94f841bb70c1818c4e4fea2d2
Sam Halliday <sam.halliday@gmail.com>
2020-09-02 19:23:04 +0100

    update elpa

--- .emacs.d/elpa/undo-tree-0.7.5/undo-tree-autoloads.el
+++ .emacs.d/elpa/undo-tree-0.7.5/undo-tree-autoloads.el
@@ -6,15 +6,19 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
-;;;### (autoloads nil "undo-tree" "../../../../.emacs.d/elpa/undo-tree-0.7.4/undo-tree.el"
-;;;;;;  "5fed979bb5eb8beaaeafabc6e34a9fd6")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/undo-tree-0.7.4/undo-tree.el
+;;;### (autoloads nil "undo-tree" "undo-tree.el" (0 0 0 0))
+;;; Generated autoloads from undo-tree.el
 
 (autoload 'undo-tree-mode "undo-tree" "\
 Toggle undo-tree mode.
 With no argument, this command toggles the mode.
 A positive prefix argument turns the mode on.
 A negative prefix argument turns it off.
+
+If called interactively, enable Undo-Tree mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
 
 Undo-tree-mode replaces Emacs' standard undo feature with a more
 powerful yet easier to use version, that treats the undo history
@@ -29,6 +33,8 @@ Within the undo-tree visualizer, the following keys are available:
   \\{undo-tree-visualizer-mode-map}
 
 \(fn &optional ARG)" t nil)
+
+(put 'global-undo-tree-mode 'globalized-minor-mode t)
 
 (defvar global-undo-tree-mode nil "\
 Non-nil if Global Undo-Tree mode is enabled.
@@ -51,20 +57,8 @@ Undo-Tree mode is enabled in all buffers where
 See `undo-tree-mode' for more information on Undo-Tree mode.
 
 \(fn &optional ARG)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "undo-tree" "../../../../.emacs.d/elpa/undo-tree-0.7.4/undo-tree.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../.emacs.d/elpa/undo-tree-0.7.4/undo-tree.el
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "undo-tree" '("undo-" "turn-on-undo-tree-mode" "*undo-tree-id-counter*" "buffer-undo-tree")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/undo-tree-0.7.4/undo-tree-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/undo-tree-0.7.4/undo-tree.el")
-;;;;;;  (0 0 0 0))
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "undo-tree" '("*undo-tree-id-counter*" "buffer-undo-tree" "turn-on-undo-tree-mode" "undo-")))
 
 ;;;***
 
