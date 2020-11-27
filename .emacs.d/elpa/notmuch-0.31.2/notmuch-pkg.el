b478e7bddf940c9601b8ba1cb249fef49aef9b45
Sam Halliday <sam.halliday@gmail.com>
2020-11-27 09:48:54 +0000

    melpa update

---
+++ .emacs.d/elpa/notmuch-0.31.2/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "0.31.2" "run notmuch within emacs" 'nil :commit "02a7b026d938f53d9fd9b183ca2b3c44c92489f2" :url "https://notmuchmail.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
