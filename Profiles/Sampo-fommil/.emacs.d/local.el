54f31fd89bf2b777931528e5a1105e00e1a03704
Sam Halliday <sam.halliday@gmail.com>
2021-10-03 12:55:52 +0100

    no active projects

--- Profiles/Sampo-fommil/.emacs.d/local.el
+++ Profiles/Sampo-fommil/.emacs.d/local.el
@@ -16,9 +16,6 @@
 ;; I'll usually want access to these..
 (find-file (expand-file-name "scratch.el" user-emacs-directory))
 
-(find-file "~/Projects/haskell-tng.el")
-(find-file "~/Projects/boilerplate")
-
 ;; flycheck is CPU intensive so only for desktops
 ;; (add-hook 'haskell-mode-hook
 ;;           (lambda ()
