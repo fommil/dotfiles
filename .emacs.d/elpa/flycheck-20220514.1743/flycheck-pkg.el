24e7634edfe524665b01be98ebf604d090ec53ab
Sam Halliday <sam.halliday@gmail.com>
2022-05-15 20:52:15 +0100

    update elpa

--- .emacs.d/elpa/flycheck-20220514.1743/flycheck-pkg.el
+++ .emacs.d/elpa/flycheck-20220514.1743/flycheck-pkg.el
@@ -1,10 +1,10 @@
-(define-package "flycheck" "20220504.830" "On-the-fly syntax checking"
+(define-package "flycheck" "20220514.1743" "On-the-fly syntax checking"
   '((dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
     (seq "1.11")
     (emacs "24.3"))
-  :commit "1d7c1b20782ccbaa6f97e37f5e1d0cee3d5eda8a" :authors
+  :commit "66a973afca1d03b8284baaa7590eb2b8615a1e6a" :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainer
   '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
