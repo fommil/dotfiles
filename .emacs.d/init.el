70df66a1fe9a06ff701629dc7617b4655d18dee7
Sam Halliday <sam.halliday@gmail.com>
2024-06-18 14:54:39 +0100

    elpa update and source code printing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -415,6 +415,29 @@ Inspired by `org-combine-plists'."
   (("s-f" . projectile-find-file)
    ("s-F" . projectile-ag)))
 
+(use-package ps-print
+  :ensure nil
+  :commands (ps-print-with-faces ps-print-preprint)
+  :init
+  (setq ps-left-header (list 'ps-get-buffer-name (lambda () (file-relative-name default-directory (projectile-project-root))))
+        ps-right-header (list "/pagenumberstring load")))
+(when (executable-find "ps2pdf")
+  (defun pdf-print-buffer-with-faces (&optional filename)
+    ;; https://emacs.stackexchange.com/a/42163/5142
+    "Print file in the current buffer as pdf, including font, color, and
+underline information.  This command works only if you are using a window system,
+so it has a way to determine color values.
+
+C-u COMMAND prompts user where to save the Postscript file (which is then
+converted to PDF at the same location."
+    (interactive (list (if current-prefix-arg
+                           (ps-print-preprint 4)
+                         (concat (file-name-sans-extension (buffer-file-name)) ".ps"))))
+    (ps-print-with-faces (point-min) (point-max) filename)
+    (shell-command (concat "ps2pdf " filename))
+    (delete-file filename)
+    (message "Wrote %s" (concat (file-name-sans-extension filename) ".pdf"))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; This section is for loading and tweaking generic modes that are
 ;; used in a variety of contexts, but can be lazily loaded based on
