8f806c59eb67f3cc6e8a386a5824d7b60a2ee6fc
Sam Halliday <sam.halliday@gmail.com>
2024-07-04 19:55:26 +0100

    laptop startup preferences

--- Profiles/Sambuca-fommil/.emacs.d/local.el
+++ Profiles/Sambuca-fommil/.emacs.d/local.el
@@ -16,6 +16,8 @@
 ;; I'll usually want access to these..
 (find-file (expand-file-name "scratch.el" user-emacs-directory))
 
+(notmuch)
+
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
 ;; End:
