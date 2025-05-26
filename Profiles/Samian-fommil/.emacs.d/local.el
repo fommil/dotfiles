a2fdd4993009ba642d3dd1b9e4aa859a5598160d
Sam Halliday <sam.halliday@gmail.com>
2025-05-26 21:07:34 +0100

    small updates

--- Profiles/Samian-fommil/.emacs.d/local.el
+++ Profiles/Samian-fommil/.emacs.d/local.el
@@ -16,7 +16,9 @@
 ;; I'll usually want access to these..
 (find-file (expand-file-name "scratch.el" user-emacs-directory))
 
-(notmuch)
+(find-file "~/Projects/luddcam/")
+
+;;(notmuch)
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
