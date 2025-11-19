dae901034e170b1ac7ce6bf93b485dbe7b5ef549
Sam Halliday <sam.halliday@gmail.com>
2025-11-19 10:30:25 +0000

    melpa update

--- .emacs.d/elpa/flycheck-20251116.2042/flycheck-pkg.el
+++ .emacs.d/elpa/flycheck-20251116.2042/flycheck-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "flycheck" "20251105.1433"
+(define-package "flycheck" "20251116.2042"
   "On-the-fly syntax checking."
   '((emacs "27.1")
     (seq   "2.24"))
   :url "https://www.flycheck.org"
-  :commit "b13548bb396cf4ed13b41fbc195c918266fb41f3"
-  :revdesc "b13548bb396c"
+  :commit "672ed3d35c7d0a386aa755bae15bb143f356923a"
+  :revdesc "672ed3d35c7d"
   :keywords '("convenience" "languages" "tools")
   :authors '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainers '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
--- .emacs.d/elpa/flycheck-20251116.2042/flycheck.el
+++ .emacs.d/elpa/flycheck-20251116.2042/flycheck.el
@@ -10,8 +10,8 @@
 ;;             Bozhidar Batsov <bozhidar@batsov.dev>
 ;; URL: https://www.flycheck.org
 ;; Keywords: convenience, languages, tools
-;; Package-Version: 20251105.1433
-;; Package-Revision: b13548bb396c
+;; Package-Version: 20251116.2042
+;; Package-Revision: 672ed3d35c7d
 ;; Package-Requires: ((emacs "27.1") (seq "2.24"))
 
 ;; This file is not part of GNU Emacs.
@@ -10886,6 +10886,15 @@ Requires Flake8 3.0 or newer. See URL
 (flycheck-def-config-file-var flycheck-python-ruff-config python-ruff
                               '("pyproject.toml" "ruff.toml" ".ruff.toml"))
 
+(defun flycheck-python-ruff-explainer (err)
+  "Return documentation for the ruff `flycheck-error' ERR."
+  (when-let (error-code (flycheck-error-id err))
+    (lambda ()
+      (flycheck-call-checker-process
+       'python-ruff nil standard-output t "rule" error-code)
+      (with-current-buffer standard-output
+        (flycheck--fontify-as-markdown)))))
+
 (flycheck-define-checker python-ruff
   "A Python syntax and style checker using Ruff.
 
@@ -10912,9 +10921,10 @@ See URL `https://docs.astral.sh/ruff/'."
           line-end)
    (warning line-start
             (or "-" (file-name)) ":" line ":" (optional column ":") " "
-            (id (one-or-more (any alpha)) (one-or-more digit) " ")
+            (id (one-or-more (any alpha)) (one-or-more digit)) " "
             (message (one-or-more not-newline))
             line-end))
+  :error-explainer flycheck-python-ruff-explainer
   :working-directory flycheck-python-find-project-root
   :modes (python-mode python-ts-mode)
   :next-checkers ((warning . python-mypy)))
@@ -12502,13 +12512,25 @@ or added as a shellcheck directive before the source command:
   :safe #'booleanp
   :package-version '(flycheck . "31"))
 
+(flycheck-def-option-var flycheck-shellcheck-infer-shell nil sh-shellcheck
+  "Whether to let ShellCheck infer the shell from the script.
+
+When non-nil, the --shell flag is not passed to ShellCheck,
+allowing it to infer the shell from the shebang line or
+shellcheck directives in the script."
+  :type 'boolean
+  :safe #'booleanp
+  :package-version '(flycheck . "36"))
+
 (flycheck-define-checker sh-shellcheck
   "A shell script syntax and style checker using Shellcheck.
 
 See URL `https://github.com/koalaman/shellcheck/'."
   :command ("shellcheck"
             "--format" "checkstyle"
-            "--shell" (eval (symbol-name sh-shell))
+            (eval
+             (unless flycheck-shellcheck-infer-shell
+               (list "--shell" (symbol-name sh-shell))))
             (option-flag "--external-sources"
                          flycheck-shellcheck-follow-sources)
             (option "--exclude" flycheck-shellcheck-excluded-warnings list
