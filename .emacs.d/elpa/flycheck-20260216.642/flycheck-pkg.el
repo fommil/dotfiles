f70f5c479805ecdadef1aabc034caaaad48fd2f8
Sam Halliday <sam.halliday@gmail.com>
2026-02-18 09:55:17 +0000

    elpa update

--- .emacs.d/elpa/flycheck-20260216.642/flycheck-pkg.el
+++ .emacs.d/elpa/flycheck-20260216.642/flycheck-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "flycheck" "20251128.1706"
+(define-package "flycheck" "20260216.642"
   "On-the-fly syntax checking."
   '((emacs "27.1")
     (seq   "2.24"))
   :url "https://www.flycheck.org"
-  :commit "62570fafbedb8fa3f7d75a50a9364feca3b294ef"
-  :revdesc "62570fafbedb"
+  :commit "891e11207fa7e3d8ec477436746c876084647a7f"
+  :revdesc "891e11207fa7"
   :keywords '("convenience" "languages" "tools")
   :authors '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainers '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
