fbf594e6f3078af1066ae9b04757717ec3253874
Sam Halliday <sam.halliday@gmail.com>
2026-08-28 12:42:41 +0100

    remove my old tags management

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -436,8 +436,6 @@ Inspired by `org-combine-plists'."
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   ;; allows using TAGS even if M-. is overruled
   (define-key projectile-command-map (kbd "j") 'xref-find-definitions)
-  ;; crutch, may not need this any more
-  (define-key projectile-command-map (kbd "R") 'fommil-regenerate-tags)
   (projectile-mode 1)
   :bind
   (("s-f" . projectile-find-file)
@@ -454,42 +452,6 @@ Inspired by `org-combine-plists'."
   (setq etags-regen-tags-file "TAGS")
   :config
   (etags-regen-mode 1))
-
-;; removed upstream https://github.com/bbatsov/projectile/pull/2041
-;; possibly replaced by etags-regen
-(defvar-local fommil-tags-command "ctags -Re -f \"%s\" \"%s\""
-  "The command used to generate a TAGS file.")
-(defun fommil-regenerate-tags ()
-  "Regenerate the project's TAGS file."
-  (interactive)
-  (let* ((command-format fommil-tags-command)
-         (default-directory (projectile-acquire-root))
-         (tags-file (expand-file-name "TAGS"))
-         (command (format command-format (or (file-remote-p tags-file 'localname) tags-file) "."))
-         shell-output exit-code)
-    (with-temp-buffer
-      (setq exit-code (process-file-shell-command command nil (current-buffer))
-            shell-output (string-trim (buffer-string))))
-    (unless (zerop exit-code)
-      (error "%s" shell-output))
-    (visit-tags-table tags-file)
-    (message "Regenerated %s" tags-file)))
-
-;; (defun fommil-visit-project-tags-table ()
-;;   "Visit the current project's tags table."
-;;   (when (projectile-project-p)
-;;     (let ((tags-file (projectile-expand-root "TAGS")))
-;;       (when (file-exists-p tags-file)
-;;         (with-demoted-errors "Error loading tags-file: %s"
-;;           (visit-tags-table tags-file t))))))
-
-;; (defun fommil-find-tag ()
-;;   "Find tag in project."
-;;   (interactive)
-;;   (fommil-visit-project-tags-table)
-;;   (call-interactively #'xref-find-definitions))
-
-;; (add-hook 'find-file-hook 'fommil-visit-project-tags-table)
 
 (use-package ps-print
   :ensure nil
